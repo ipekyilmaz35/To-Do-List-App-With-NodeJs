38601a15f5be4d02934fbbb4e29f5d2bcffa8915
ipekyilmaz35 <ipekyi35@gmail.com>
2024-02-09 14:42:38 +0100

    app.js: change of PORT  and the place of the let todos

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+let todos = [];
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-
-let todos = [];
 
 app.get('/', (req, res) => {
     res.render('index', { todos });
